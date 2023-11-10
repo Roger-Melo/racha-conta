d9aa975d0d2dcfca7a72c52c47dc1b028a730e99
Roger-Melo <rogerw592@gmail.com>
2023-11-10 20:52:28 -0300

    ✅ Add lógica de rachar conta

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const initialFriends = [
   { id: crypto.randomUUID(), name: 'André', balance: -7, img: 'friends/andre-48.jpg' },
   { id: crypto.randomUUID(), name: 'Pri', balance: 20, img: 'friends/pri-48.jpg' },
@@ -10,46 +12,78 @@ const getMsgInfo = balance => balance < 0
     ? { message: `Te deve ${balance} reais`, color: 'green-credit' }
     : { message: 'Estão quites', color: 'white-neutral' }
 
-const App = () => (
-  <main className="app">
-    <aside className="sidebar">
-      <ul>
-        {initialFriends.map(friend => {
-          const { message, color } = getMsgInfo(friend.balance)
+const App = () => {
+  const [friends, setFriends] = useState(initialFriends)
+  const [selectedFriend, setSelectedFriend] = useState(null)
+  const [totalBill, setTotalBill] = useState('100')
+  const [mySpend, setMySpend] = useState('50')
+  const [whoWillPay, setWhoWillPay] = useState('you')
 
-          return (
-            <li key={friend.id}>
-              <img src={friend.img} alt={`Foto de ${friend.name}`} />
-              <h3>{friend.name}</h3>
-              <p className={color}>{message}</p>
-              <button className="button">Selecionar</button>
-            </li>
-          )
-        }
-        )}
-      </ul>
-    </aside>
+  const handleClickFriend = friend => setSelectedFriend(p => p?.id === friend.id ? null : friend)
+  const handleChangeBill = e => setTotalBill(e.target.value)
+  const handleChangeMySpend = e => setMySpend(e.target.value)
+  const handleChangeWhoWillPay = e => setWhoWillPay(e.target.value)
 
-    <form className="form-split-bill">
-      <h2>Rache a conta com _NOME_</h2>
-      <label>
-        💰 Valor total
-        <input type="number" defaultValue={100} />
-      </label>
-      <label>
-        🤸🏻‍♂️ Seus gastos
-        <input type="number" defaultValue={50} />
-      </label>
-      <label>
-        🤑 Quem vai pagar
-        <select>
-          <option value='you'>Você</option>
-          <option value='_NOME_'>_NOME_</option>
-        </select>
-      </label>
-      <button className="button">Rachar conta</button>
-    </form>
-  </main>
-)
+  const handleSubmitShareBill = e => {
+    e.preventDefault()
+    setFriends(prev => prev.map(friend => selectedFriend.id === friend.id
+      ? {
+        ...friend,
+        balance: whoWillPay === 'you'
+          ? friend.balance + (+totalBill - +mySpend)
+          : friend.balance - mySpend
+      }
+      : friend
+    ))
+  }
+
+  return (
+    <main className="app">
+      <aside className="sidebar">
+        <ul>
+          {friends.map(friend => {
+            const { message, color } = getMsgInfo(friend.balance)
+            const isSelectedFriend = friend.id === selectedFriend?.id
+
+            return (
+              <li key={friend.id}>
+                <img src={friend.img} alt={`Foto de ${friend.name}`} />
+                <h3>{friend.name}</h3>
+                <p className={color}>{message}</p>
+                <button
+                  onClick={() => handleClickFriend(friend)}
+                  className={`button ${isSelectedFriend ? 'button-close' : ''}`}
+                >
+                  {isSelectedFriend ? 'Fechar' : 'Selecionar'}
+                </button>
+              </li>
+            )
+          }
+          )}
+        </ul>
+      </aside>
+
+      {selectedFriend && <form onSubmit={handleSubmitShareBill} className="form-split-bill">
+        <h2>Rache a conta com {selectedFriend.name}</h2>
+        <label>
+          💰 Valor total
+          <input value={totalBill} onChange={handleChangeBill} type="number" />
+        </label>
+        <label>
+          🤸🏻‍♂️ Seus gastos
+          <input value={mySpend} onChange={handleChangeMySpend} type="number" />
+        </label>
+        <label>
+          🤑 Quem vai pagar
+          <select value={whoWillPay} onChange={handleChangeWhoWillPay}>
+            <option value='you'>Você</option>
+            <option value={selectedFriend.name}>{selectedFriend.name}</option>
+          </select>
+        </label>
+        <button className="button">Rachar conta</button>
+      </form>}
+    </main>
+  )
+}
 
 export { App }
